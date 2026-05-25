8468bd58608327864a254d3f96c9708df72697fe
Anuko320 <kairatanuarbek00@gmail.com>
2026-05-25 14:30:18 +0500

    Add users search functionality

--- src/app/features/users/pages/users-list/users-list.ts
+++ src/app/features/users/pages/users-list/users-list.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { UsersService } from '../../services/users';
 import { User } from '../../../../core/models/user';
@@ -7,7 +8,7 @@ import { User } from '../../../../core/models/user';
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './users-list.html',
   styleUrl: './users-list.scss',
 })
@@ -15,12 +16,22 @@ export class UsersList implements OnInit {
   private usersService = inject(UsersService);
 
   users: User[] = [];
+  filteredUsers: User[] = [];
+
+  search = '';
 
   ngOnInit(): void {
-  this.usersService.getUsers().subscribe((users) => {
-    console.log(users);
+    this.usersService.getUsers().subscribe((users) => {
+      console.log(users);
 
-    this.users = users;
-  });
+      this.users = users;
+      this.filteredUsers = users;
+    });
+  }
+
+  onSearch(): void {
+    this.filteredUsers = this.users.filter((user) =>
+      user.name.toLowerCase().includes(this.search.toLowerCase())
+    );
   }
 }
