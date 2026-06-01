ec3911b0a88bd94c338314c98d06433bca921712
Anuko320 <kairatanuarbek00@gmail.com>
2026-06-01 12:56:53 +0500

    refactor: reorganize imports and enhance logout functionality
    - Moved the router and routes imports to the correct position in app.config.ts for better clarity.
    - Updated app.html to conditionally render the logout button based on authentication status.
    - Refactored logout method in app.ts to utilize AuthService for improved logout handling and navigation to the login page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 import { ThemeToggle } from './core/components/theme-toggle/theme-toggle';
 import { ThemeService } from './core/services/theme.service';
 
 import { LanguageToggle } from './core/components/language-toggle/language-toggle';
 import { LanguageService } from './core/services/language.service';
+
+import { AuthService } from './core/services/auth';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +16,22 @@ import { LanguageService } from './core/services/language.service';
   styleUrl: './app.scss',
 })
 export class App {
+  readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
   readonly languageService = inject(LanguageService);
+
+  readonly isAuthenticated = computed(() =>
+    this.authService.isAuthenticated()
+  );
 
   constructor() {
     inject(ThemeService);
   }
+
   logout(): void {
-    localStorage.removeItem('token');
-    location.reload();
+    this.authService.logout();
+    void this.router.navigate(['/login']);
   }
 }
 
